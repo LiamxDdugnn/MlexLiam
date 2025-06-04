563e4d7ab308b6396d0ff891537399d214ba8736
phạm tiến dũng <tdungx02@gmail.com>
2025-06-04 11:47:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function openPopup() {
-  document.getElementById('popup').style.display = 'block';
+function openPopup(qr, bank, title) {
+  document.getElementById('qr-img').src = qr;
+  document.getElementById('bank-img').src = bank;
+  document.getElementById('popup-title').textContent = title;
+  document.getElementById('popup').style.display = 'flex';
 }
 
 function closePopup() {
